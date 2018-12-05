77a421928cf86c8887a81e0f424345c3d9716982
桔子 <bollwarm@tom.com>
2018-12-05 19:34:00 +0800

    mew

--- getData.py
+++ getData.py
@@ -1,14 +1,14 @@
 import urllib.request
 import json
 
-opener = urllib.request.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+sop = urllib.request.build_opener()
+sop.addheaders = [('User-agent', 'Mozilla/5.0')]
 
 #SEND QUERY (IN THE FUNCTION)
-fp=opener.open('http://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=40.0952&lng=116.63349&fDstL=0&fDstU=20')
-mybyte=fp.read()
-mystr=mybyte.decode("utf8")
-js_str=json.loads(mystr)
+fp=sop.open('http://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=40.0952&lng=116.63349&fDstL=0&fDstU=20')
+ccbyte=fp.read()
+jsdata=ccbyte.decode("utf8")
+cisinfo=json.loads(jsdata)
 fp.close()
 
-print(js_str)
+print(cisinfo)
